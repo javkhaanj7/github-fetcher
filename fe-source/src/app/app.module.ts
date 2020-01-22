f65a7083d178444afe49cbea6f7222c81306d6a4
Javkhlantugs Nyamdorj <javkhaanj7@gmail.com>
2020-01-22 17:22:25 +0800

    Fix base href for local development

--- fe-source/src/app/app.module.ts
+++ fe-source/src/app/app.module.ts
@@ -2,7 +2,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { APP_BASE_HREF } from '@angular/common';
 
 import { ToastrModule } from 'ngx-toastr';
 
@@ -26,7 +25,7 @@ import { ApiService } from './services/api.service';
     AppRoutingModule,
     GraphQLModule
   ],
-  providers: [{ provide: APP_BASE_HREF, useValue: '/github-fetcher' }, ApiService],
+  providers: [ApiService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
